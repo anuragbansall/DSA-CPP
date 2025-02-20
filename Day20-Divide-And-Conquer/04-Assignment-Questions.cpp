d3569cfdbe2c2d63fe3575cb1df80825bbc98b46
Anurag Bansal <devanuragbansal@gmail.com>
2025-02-20 17:20:09 +0530

    Assignment Ques Completed till majorityElement()

--- Day20-Divide-And-Conquer/04-Assignment-Questions.cpp
+++ Day20-Divide-And-Conquer/04-Assignment-Questions.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
+
+void printArray(vector<string> &arr);
+void merge(vector<string> &arr, int start, int mid, int end);
+void mergeSort(vector<string> &arr, int start, int end);
+int majorityElement(vector<int> &nums, int start, int end);
+int countInRange(vector<int> &nums, int start, int end, int target);
 
 int main(void)
 {
@@ -10,6 +17,14 @@ int main(void)
         Sample Input 1: arr = {"sun", "earth", "mars", "mercury"}
         Sample Output 1: arr = {"earth", "mars", "mercury", "sun"}
     */
+    vector<string> arr = {"sun", "earth", "mars", "mercury"};
+
+    cout << "Before Sorting: ";
+    printArray(arr);
+
+    cout << "After Sorting: ";
+    mergeSort(arr, 0, arr.size() - 1);
+    printArray(arr);
 
     /*
         Question 2: Given an array nums of size n, return the majority element. (MEDIUM)
@@ -24,6 +39,9 @@ int main(void)
         ● 1 <= n <= 5 * 10^4
         ● -10^9 <= nums[i] <= 10^9
     */
+    vector<int> nums = {2, 2, 1, 1, 1, 2, 2};
+    int majority = majorityElement(nums, 0, nums.size() - 1);
+    cout << "Majority Element: " << majority << endl;
 
     /*
         Question 3: Given an array of integers. Find the Inversion Count in the array. (HARD)
@@ -42,4 +60,99 @@ int main(void)
     */
 
     return 0;
+}
+
+void printArray(vector<string> &arr)
+{
+    for (string s : arr)
+    {
+        cout << s << " ";
+    }
+    cout << endl;
+}
+
+void merge(vector<string> &arr, int start, int mid, int end)
+{
+    int i = start;
+    int j = mid + 1;
+
+    vector<string> temp;
+
+    while (i <= mid && j <= end)
+    {
+        if (arr[i] < arr[j])
+        {
+            temp.push_back(arr[i++]);
+        }
+        else
+        {
+            temp.push_back(arr[j++]);
+        }
+    }
+
+    while (i <= mid)
+    {
+        temp.push_back(arr[i++]);
+    }
+    while (j <= end)
+    {
+        temp.push_back(arr[j++]);
+    }
+
+    for (int i = start, j = 0; i <= end; i++)
+    {
+        arr[i] = temp[j++];
+    }
+}
+void mergeSort(vector<string> &arr, int start, int end)
+{
+    if (start >= end)
+    {
+        return;
+    }
+
+    int mid = start + (end - start) / 2;
+
+    mergeSort(arr, start, mid);
+    mergeSort(arr, mid + 1, end);
+
+    merge(arr, start, mid, end);
+}
+
+int countInRange(vector<int> &nums, int start, int end, int target)
+{
+    int count = 0;
+    for (int i = start; i <= end; i++)
+    {
+        if (nums[i] == target)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int majorityElement(vector<int> &nums, int start, int end)
+{
+    if (start == end)
+    {
+        return nums[start];
+    }
+
+    int mid = start + (end - start) / 2;
+
+    int leftMajority = majorityElement(nums, start, mid);
+    int rightMajority = majorityElement(nums, mid + 1, end);
+
+    if (leftMajority == rightMajority) // If both the halves have the same majority element
+    {
+        return leftMajority;
+    }
+    else // If both the halves have different majority elements
+    {
+        int leftCount = countInRange(nums, start, end, leftMajority);
+        int rightCount = countInRange(nums, start, end, rightMajority);
+
+        return leftCount > rightCount ? leftMajority : rightMajority;
+    }
 }
