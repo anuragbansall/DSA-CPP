aa20a36f10de642268590097c533d7a282dc065d
Anurag Bansal <devanuragbansal@gmail.com>
2025-09-02 17:20:28 +0530

    Implement string decoding functionality with stack data structure

--- Day27-Stack-II/05-Assignment-Questions.cpp
+++ Day27-Stack-II/05-Assignment-Questions.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stack>
+#include <string>
 using namespace std;
 
 class ListNode
@@ -12,6 +13,7 @@ public:
 };
 
 bool isPalindrome(ListNode *head);
+string decodeString(const string &s);
 
 int main(void)
 {
@@ -30,6 +32,18 @@ int main(void)
     head->next->next->next = new ListNode(1);
 
     cout << (isPalindrome(head) ? "true" : "false") << endl;
+
+    /*
+        Question 2: Given an encoded string, return its decoded string.
+        The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is repeated exactly k times.
+        Note that k is a positive integer. You may assume the input string is always valid: no extra whitespaces, square brackets are well-formed, etc.
+        Also, the original data does not contain any digits; digits are only used for repeat counts k.
+        For example, there will not be inputs like 3a or 2[4].
+
+        Examples:
+        Input: s = "3[a]2[bc]"      Output: "aaabcbc"
+        Input: s = "2[abc]3[cd]ef"  Output: "abcabccdcdcdef"
+    */
 
     return 0;
 }
@@ -59,4 +73,47 @@ bool isPalindrome(ListNode *head)
     }
 
     return true;
+}
+
+string decodeString(const string &s)
+{
+    stack<int> countStack;
+    stack<string> stringStack;
+    string currentString;
+    int n = 0;
+
+    for (char c : s)
+    {
+        if (isdigit(c))
+        {
+            n = n * 10 + (c - '0'); // This builds the repeat count
+        }
+        else if (c == '[')
+        {
+            countStack.push(n);
+            n = 0;
+            stringStack.push(currentString);
+            currentString.clear();
+        }
+        else if (c == ']')
+        {
+            int count = countStack.top();
+            countStack.pop();
+
+            string decodedString;
+            for (int i = 0; i < count; ++i)
+            {
+                decodedString += currentString;
+            }
+
+            currentString = stringStack.top() + decodedString;
+            stringStack.pop();
+        }
+        else
+        {
+            currentString += c;
+        }
+    }
+
+    return currentString;
 }
